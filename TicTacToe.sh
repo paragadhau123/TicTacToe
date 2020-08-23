ee29857f0c9c58526d363e71897faa7c4a8f83dc
parag <adhauparag64@gmail.com>
2020-08-24 00:42:49 +0530

    Added display the board

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -9,15 +9,16 @@ COMPUTER="O"
 
 function resetTheBoard()
 {
+ count=1;
  for (( i=1; i<=9; i++ ))
  do
    boardOfTicTacToe["$i"]="-";
+   count=$(($count+1))
  done
+ displayBoard
 }
 
-resetTheBoard
-
-echo ${boardOfTicTacToe[@]}
+#echo ${boardOfTicTacToe[@]}
 
 function whoPlayFirst()
 {
@@ -25,8 +26,30 @@ function whoPlayFirst()
  if [ $resultOfToss -eq 0 ]
  then
      echo "Player play First....."
+     resetTheBoard
+     play
  else
      echo "Computer play First..."
  fi
 }
+
+function play()
+{
+ read -p "Enter the Position to Insert the "X" : " position
+ boardOfTicTacToe["$position"]="X"
+}
+
+function displayBoard()
+{
+   echo "    |---|---|---|"
+   echo "    | "${boardOfTicTacToe[1]}" | "${boardOfTicTacToe[2]}" | "${boardOfTicTacToe[3]}" |"
+   echo "    |---|---|---|"
+   echo "    | "${boardOfTicTacToe[4]}" | "${boardOfTicTacToe[5]}" | "${boardOfTicTacToe[6]}" |"
+   echo "    |---|---|---|"
+   echo "    | "${boardOfTicTacToe[7]}" | "${boardOfTicTacToe[8]}" | "${boardOfTicTacToe[9]}" |"
+   echo "    |---|---|---|"
+
+}
+resetTheBoard
 whoPlayFirst
+displayBoard
