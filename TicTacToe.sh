ea528e9ab8369e5ce5c6eff9b25eda6b006f2772
parag <adhauparag64@gmail.com>
2020-08-24 00:04:21 +0530

    Added reset function

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -1,2 +1,17 @@
 #!/bin/bash -x
+
 echo "*************************Welcome to TICTACTOE WORLD****************************"
+
+declare -a boardOfTicTacToe
+
+function resetTheBoard()
+{
+ for (( i=1; i<=9; i++ ))
+ do
+   boardOfTicTacToe["$i"]="-";
+ done
+}
+
+resetTheBoard
+
+echo ${boardOfTicTacToe[@]}
