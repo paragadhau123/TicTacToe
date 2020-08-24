e307843e96a79559355b6a4135f0f6409a93d128
parag <adhauparag64@gmail.com>
2020-08-24 15:47:35 +0530

    Added condition if computer can win then play that move

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -9,7 +9,7 @@ computer=''
 won=false
 number=0
 switchPlayers=false
-
+computerWinMove=false
 declare -a boardOfTicTacToe
 
 function resetTheBoard()
@@ -17,8 +17,7 @@ function resetTheBoard()
 local count=1;
  for (( i=1; i<=$NUM_OFPOSITION; i++ ))
  do
-   boardOfTicTacToe[$i]=$count1;
-   count=$(($count1+1))
+   boardOfTicTacToe[$i]='-'
  done
 
 }
@@ -73,21 +72,64 @@ function player() {
             fi
 	fi
 }
+function checkToWin()
+{
+	counter=1
+	if [ $computerWinMove=false ]
+	then
+		for (( i=1; i<=3; i++ ))
+		do
+			if [[ ${boardOfTicTacToe[$counter]} == ${boardOfTicTacToe[$counter+$1+$1]} ]] && [[ ${boardOfTicTacToe[$counter+$1]} == '-' ]] && [[ ${boardOfTicTacToe[$counter]} == $3 ]]
+			then
+				computerPosition=$(($counter+$1))
+				boardOfTicTacToe[$computerPosition]=$computer
+				computerWinMove=true
+                                displayBoard
+				break
+			elif [[  ${boardOfTicTacToe[$counter]} == ${boardOfTicTacToe[$counter+$1]} ]] && [[  ${boardOfTicTacToe[$counter+$1+$1]} == '-' ]] && [[ ${boardOfTicTacToe[$counter]} == $3 ]]
+			then
+				computerPosition=$(($counter+$1+$1))
+				boardOfTicTacToe[$computerPosition]=$computer
+				computerWinMove=true
+                                displayBoard
+				break
+			elif [[ ${boardOfTicTacToe[$counter+$1]} == ${boardOfTicTacToe[$counter+$1+$1]} ]] && [[ ${boardOfTicTacToe[$counter]} == '-' ]] && [[ ${boardOfTicTacToe[$counter+$1]} == $3 ]]
+			then
+				computerPosition=$counter
+				boardOfTicTacToe[$computerPosition]=$computer
+				computerWinMove=true
+                                displayBoard 
+				break
+			fi
+			counter=$(($counter+$2))
+		done
+	fi
+}
 function computer() {
 	echo "Computer's Turn"
-	positionForComputer=$(( RANDOM%9 + 1 ))
+	rowValue=1
+	columnValue=3
+	leftDiagonalValue=4
+	rightDiagonalValue=2
+
+	checkToWin $rowValue $columnValue $1
+	checkToWin $columnValue $rowValue $1
+	checkToWin $leftDiagonalValue 0 $1
+	checkToWin $rightDiagonalValue 0 $1
+        if [ $computerWinMove = false ]
+        then
+            positionForComputer=$(( RANDOM%9+1 ))
             if  [[ ${boardOfTicTacToe["$positionForComputer"]} == "$player" ]] || [[ ${boardOfTicTacToe["positionForComputer"]} == "$computer" ]]
             then
                 echo "Please Select Another Block....Already Occupied!!!!!!!!!!"
                 computer
-                displayBoard
-            else 
+            else
 
 		boardOfTicTacToe["$positionForComputer"]="$computer"
 		displayBoard
             fi
 
-}
+          }
 function winInRowsAndColumns() {
 	startPosition=1
 	while [ $startPosition -le 3 ]
@@ -95,10 +137,11 @@ function winInRowsAndColumns() {
 		if [[ ${boardOfTicTacToe[$startPosition]} == $1 ]] && [[ ${boardOfTicTacToe[$startPosition+$2]} == $1 ]] &&  [[ ${boardOfTicTacToe[$startPosition+$2+$2]} == $1 ]]
 		then
 			echo "$1 Winner!"
+                        displayBoard
 			won=true
 			break
 		else
-			startPosition=$(( $startPosition + 3 ))
+			startPosition=$(( $startPosition + $3 ))
 		fi
 	done
         echo $won
@@ -125,21 +168,19 @@ function winInDiagonals() {
 }
 function play(){
 local temp=1;
-	while [[ $( winInRowsAndColumns $won ) == false ]] || [[ $( winInDiagonals $won ) == false ]]
-	do
+	while [[ $won != true ]]
+do
                 echo Hiii
 		rowBlock=1
 		columnBlock=3
 	        if [ $switchPlayers == false ]
                 then
                     player
-                    winInRowsAndColumns $player $rowBlock  $won
-                    winInRowsAndColumns $player $columnBlock $rowBlock $won
-                    winInDiagonals $player $won
+                    winInRowsAndColumns $player $rowBlock  $columnBlock
+                    winInRowsAndColumns $player $columnBlock $rowBlock
+                    winInDiagonals $player
                     if [ $temp -ge 9 ]
                     then
-                        echo "Game Tie"
-                        won=true
                         displayBoard
                         break
                     fi
@@ -147,16 +188,14 @@ local temp=1;
       	        elif [ $switchPlayers == true ]
         	then
                	    computer
-                    winInRowsAndColumns $computer $rowBlock $won
-                    winInRowsAndColumns $computer $columnBlock $won
+                    winInRowsAndColumns $computer $rowBlock $columnBlock $won
+                    winInRowsAndColumns $computer $columnBlock $rowBlock $won
                     winInDiagonals $computer $won
                     if [ $temp -ge 9 ]
                     then
-                        echo "Game Tie"
-                        won=true
                         displayBoard
                         break
-                    fi 
+                    fi
                     switchPlayers=false
 
                 fi
